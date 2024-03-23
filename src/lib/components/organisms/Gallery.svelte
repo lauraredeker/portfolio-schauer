374c50e78eb12de76f161fa5247e6709875371ff
Laura A. Redeker <lauraredeker.ux@gmail.com>
2024-03-23 01:52:43 +0100

    add fonts via @fontsource (wip), add gallery draft

---
+++ src/lib/components/organisms/Gallery.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import image1 from '$lib/assets/images/noise-shapes/noise-shapes-01.png'
+	import image2 from '$lib/assets/images/noise-shapes/noise-shapes-02.png'
+	import image3 from '$lib/assets/images/noise-shapes/noise-shapes-03.png'
+	import image4 from '$lib/assets/images/noise-shapes/noise-shapes-04.png'
+
+	import { IconArrowLeft, IconArrowRight } from '@tabler/icons-svelte'
+
+	let images = [image1, image2, image3, image4]
+	let scrollContainer: HTMLElement
+
+	function scrollLeft() {
+		scrollContainer.scrollBy({ left: -700, behavior: 'smooth' })
+	}
+
+	function scrollRight() {
+		scrollContainer.scrollBy({ left: 700, behavior: 'smooth' })
+	}
+</script>
+
+<div class="relative flex h-full">
+	<div class="absolute right-10 top-10 z-50">
+		<button
+			class="button bg-white"
+			on:click={scrollLeft}>
+			<IconArrowLeft
+				size={24}
+				strokeWidth="1.5" />
+		</button>
+		<button
+			class="button bg-white"
+			on:click={scrollRight}>
+			<IconArrowRight
+				size={24}
+				strokeWidth="1.5" />
+		</button>
+	</div>
+	<div
+		class="gallery relative flex h-[80vh] overflow-x-scroll px-56"
+		bind:this={scrollContainer}>
+		<div
+			class="text-accent text-uppercase absolute left-12 top-20 z-10 mr-24 w-32 text-pretty text-xl text-white">
+			Scroll to the left
+			<IconArrowLeft
+				size={64}
+				strokeWidth="1.5" />
+		</div>
+
+		{#each images as image, i}
+			<img
+				src={image}
+				alt="Generative art"
+				class="z-10 h-full" />
+		{/each}
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
 	import { isPreviewing, Content } from '@builder.io/sdk-svelte'
 	import { Title } from '$lib/Constants.js'
 
+	import Gallery from '$lib/components/organisms/Gallery.svelte'
+
 	import design1 from '$lib/assets/images/noise-shapes/noise-shapes-01.png'
 
 	// this data comes from the function in `+page.server.js`, which runs on the server only
@@ -19,41 +21,27 @@
 <main class="min-h-screen w-full">
 	<section class="flex min-h-screen items-center">
 		<div class="main-container">
-			<h1 class="display-text left-20 top-24 z-20 text-left dark:text-white">
-				I create<br />
-				generat<span
-					class="bg-black font-normal italic text-white
-			  ">&#123;if&#125;</span
-				><br />
-				<span class="font-normal italic">art</span>
-			</h1>
+			<div class="flex items-center justify-center">
+				<h1 class="display-text text-left md:-ml-44 dark:text-white">
+					I create<br />
+					generat<span
+						class="bg-black font-normal italic text-white
+				  ">&#123;if&#125;</span
+					><br />
+					<span class="font-normal italic">art</span>
+				</h1>
+			</div>
 		</div>
 
 		<!--<h2 class="ml-20 mt-80 text-pretty leading-10">
 					{data.content?.data?.description || 'Description'}
 				</h2>-->
 		<img
-			class="fixed -top-40 left-20 h-[160%] opacity-60 mix-blend-darken dark:mix-blend-difference"
+			class="fixed -top-40 left-20 -z-40 aspect-auto opacity-60 mix-blend-darken md:h-[160%] dark:mix-blend-difference"
 			src={design1} />
 	</section>
 
-	<div class="grid grid-cols-3 overflow-x-scroll">
-		<div>
-			<img
-				src={design1}
-				class="w-full rotate-90" />
-		</div>
-
-		<div>
-			<img
-				src={design1}
-				class="w-full rotate-90" />
-		</div>
-
-		<div>
-			<img
-				src={design1}
-				class="w-full rotate-90" />
-		</div>
-	</div>
+	<section class="min-h-screen bg-black py-20 text-white">
+		<Gallery />
+	</section>
 </main>
